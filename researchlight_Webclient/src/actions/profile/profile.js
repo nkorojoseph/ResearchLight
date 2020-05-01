64a469f850340c0aa808189ae907fd6fdcdade10
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2020-05-01 07:30:58 +0100

    profile edited

--- researchlight_Webclient/src/actions/profile/profile.js
+++ researchlight_Webclient/src/actions/profile/profile.js
@@ -38,7 +38,7 @@ export const createProfile = (formData,history,edit=false) => async dispatch => 
             type: GET_PROFILE,
             payload: res.data
         })
-        dispatch(setAlert(edit?'Profile updated':'Profile created'))
+        dispatch(setAlert(edit?'Profile updated':'Profile created','success'))
         if(!edit){
             history.push('/dashboard')
         }
