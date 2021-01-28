c98da35ebb37ec2204a2a3893a4e1ac544802a3d
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2021-01-28 09:55:15 +0100

    gr

--- researchLight_Backend/server/server.js
+++ researchLight_Backend/server/server.js
@@ -7,11 +7,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-//test server connection
-// app.get('/', (req, res) => {
-//     res.send('Server setup done properly');
-// });
-
 //connect to db
 connectDB()
 
@@ -19,20 +14,16 @@ connectDB()
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
-//test server connection
-// app.get('/', (req, res) => {
-//     res.send('Server setup done properly');
-// });
 
 //Serve static assets in production
-if(process.env.NODE_ENV === 'production'){
+// if(process.env.NODE_ENV === 'production'){
 
     app.use(express.static('../../researchlight_Webclient/build'))
 
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'researchlight_Webclient','build','index.html'))
     })
-} 
+
 
 ///define routes
 app.use('/api/users',require('../routes/api/users'))
@@ -40,6 +31,5 @@ app.use('/api/auth',require('../routes/api/auth'))
 app.use('/api/posts',require('../routes/api/posts'))
 app.use('/api/profile',require('../routes/api/profile'))
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server running on PORT:',PORT));
