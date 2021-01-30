8db303ab305971bc0024e85e6331e67ed4e4e602
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2021-01-30 16:32:56 +0100

    package.json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDB = require('../config/db')
+const connectDB = require('./config/db')
 const bodyparser = require('body-parser')
 const path = require('path');
 const { json } = require('body-parser');
@@ -17,15 +17,15 @@ app.use(express.urlencoded({ extended: false }));
 
 
 ///define routes
-app.use('/api/users',require('../routes/api/users'))
-app.use('/api/auth',require('../routes/api/auth'))
-app.use('/api/posts',require('../routes/api/posts'))
-app.use('/api/profile',require('../routes/api/profile'))
+app.use('/api/users',require('./routes/api/users'))
+app.use('/api/auth',require('./routes/api/auth'))
+app.use('/api/posts',require('./routes/api/posts'))
+app.use('/api/profile',require('./routes/api/profile'))
 
 //Serve static assets in production
  if(process.env.NODE_ENV === 'production'){
 
-    app.use(express.static('../client/build'))
+    app.use(express.static('client/build'))
 
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname,'client', 'build','index.html'))
