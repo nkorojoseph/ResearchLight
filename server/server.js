63afda6d53d91815ab5fce2223639457491e5bf4
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2021-01-30 16:09:16 +0100

    Prepared for deployment

--- server/server.js
+++ server/server.js
@@ -16,22 +16,22 @@ app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
 
-//Serve static assets in production
-// if(process.env.NODE_ENV === 'production'){
-
-    app.use(express.static(path.join(__dirname,'build')))
-
 ///define routes
 app.use('/api/users',require('../routes/api/users'))
 app.use('/api/auth',require('../routes/api/auth'))
 app.use('/api/posts',require('../routes/api/posts'))
 app.use('/api/profile',require('../routes/api/profile'))
 
+//Serve static assets in production
+ if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('../client/build'))
+
     app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname,'build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client', 'build','index.html'))
     })
 
-
+ }
 
 
 
